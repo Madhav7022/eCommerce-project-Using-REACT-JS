db30a22a55e8f6699f24582b2a25c07af81ac40e
MADHAV JADHAV <92084941+Madhav7022@users.noreply.github.com>
2026-02-17 16:19:09 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Madhav7022 eCommerce Project 🚀</h1>
+      <p>Docker + Jenkins CI/CD Working Successfully!</p>
+    </div>
+  );
+}
+
+export default App;
